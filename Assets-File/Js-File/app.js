bbab1b4906723557b70568b8e86d9fe680508d98
Muhammed-Nayeem <mohammednayeem808@gmail.com>
2021-01-27 18:10:38 +0600

    Withdraw Section Addeded

--- Assets-File/Js-File/app.js
+++ Assets-File/Js-File/app.js
@@ -8,26 +8,32 @@ loginButton.addEventListener("click", function () {
 });
 
 //Deposit Button Event Handler:
-const depositButton = document.getElementById('depositButton');
-depositButton.addEventListener('click', function() {
-  const depositNumber = getInputNumbers('depositAmount');
+const depositButton = document.getElementById("depositButton");
+depositButton.addEventListener("click", function () {
+  const depositNumber = getInputNumbers("depositAmount");
 
   //Updated Current Deposit Amount By Calling Function:
-  updatedSpanText('currentDeposit', depositNumber);
-  //Updated Current Balance By Calling Function:
-  updatedSpanText('currentBalance', depositNumber);
+  updatedSpanText("currentDeposit", depositNumber);
+  //Updated Total Balance By Calling Function:
+  updatedSpanText("currentBalance", depositNumber);
 
   //Again set empty input field after adding value:
-  document.getElementById('depositAmount').value = "";
+  document.getElementById("depositAmount").value = "";
 });
 
 //Withdraw Button Event Handler:
-const withdrawButton = document.getElementById('withdrawButton');
-withdrawButton.addEventListener('click', function() {
-  const withdrawNumber = getInputNumbers('withdrawAmount');
-  console.log(withdrawNumber);
-});
+const withdrawButton = document.getElementById("withdrawButton");
+withdrawButton.addEventListener("click", function () {
+  const withdrawNumber = getInputNumbers("withdrawAmount");
 
+  //Updated withdraw Amount By Calling Function:
+  updatedSpanText("currentWithdraw", withdrawNumber);
+  //Updated withdraw Balance By Calling Function:
+  updatedSpanText("currentBalance", -1 * withdrawNumber);
+
+  //Again set empty input field after adding value:
+  document.getElementById("withdrawAmount").value = "";
+});
 
 //Common Function For Common Codes:
 function getInputNumbers(id) {
@@ -41,5 +47,5 @@ function updatedSpanText(id, number) {
   const current = document.getElementById(id).innerText;
   const currentNumber = parseFloat(current);
   const totalBalance = currentNumber + number;
-  document.getElementById(id).innerText = totalBalance; 
+  document.getElementById(id).innerText = totalBalance;
 }
